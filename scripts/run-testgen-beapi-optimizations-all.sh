b89cc1449e25e8e1e4f05f4ba4272dc1f7742348
Mariano Politano <marianopolitano@gmail.com>
2023-01-04 01:10:01 -0300

    fix script for run all optimization

--- scripts/run-testgen-beapi-optimizations-all.sh
+++ scripts/run-testgen-beapi-optimizations-all.sh
@@ -6,11 +6,12 @@ source $scriptsdir/scripts.sh
 
 clean_results_folders
 
-./beapi-optimizations-0_korat.sh
-./beapi-optimizations-1_kiasan.sh
-./beapi-optimizations-2_roops.sh
-./beapi-optimizations-3_fajita.sh
-./beapi-optimizations-4_real_world.sh
+./run-testgen-beapi-optimizations-0_korat.sh
+./run-testgen-beapi-optimizations-1_kiasan.sh
+./run-testgen-beapi-optimizations-2_roops.sh
+./run-testgen-beapi-optimizations-3_fajita.sh
+./run-testgen-beapi-optimizations-4_real_world.sh
+
 
 
 # process_results_beapi_vs_korat;
