7a93a764f086b7327ef9b915d410dbf76f6d2bdb
Mariano Politano <marianopolitano@gmail.com>
2023-01-05 19:07:09 -0300

    add new script for own cases

---
+++ scripts/run-builder-identification-beapi.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+projectsdir=$BE_EXP_SRC
+scriptsdir=$projectsdir/scripts
+source $scriptsdir/process-results.sh
+
+projects="$1"
+casestudy="$2"
+scope=$3
+
+TO=60m
+function run_identification(){
+    for project in $projects
+    do
+        for case in $casestudy 
+        do
+            echo "************"
+            run_identify_builders $project $case $scope
+            echo "************"
+
+        done
+    done
+}
+
+function run_identify_builders(){
+    project=$1
+    casestudy=$2
+    scope=$3
+    pushd $BE_EXP_SRC/$project
+    ant
+    #For Infer.
+    mkdir -p "$BE_EXP_SRC/src/" && cp -r src/main/java/* $BE_EXP_SRC/src
+    mkdir -p "$BE_EXP_SRC/build/classes" && cp -r build/classes/* $BE_EXP_SRC/build/classes
+    #Log for builders tools.
+    popd
+    pushd $BE_EXP_SRC
+    rm -r tmp/$casestudy/*
+    mkdir -p tmp/$casestudy/
+    log=tmp/$casestudy/console.log
+    log=$BE_EXP_SRC/tmp/$casestudy/console.log
+    if [ -h log ] && [ ! -f log ]; then
+        rm log
+    fi
+    SECONDS=0
+
+    cp $scriptsdir/literals/literals$scope.txt resources/literals.txt
+    cp $scriptsdir/properties/scope$scope.all.canonicalizer.properties resources/scope.canonicalizer.properties
+
+    cmd="java -cp $project/build/classes/:lib/korat.jar:lib/identificationBuilders.jar main.Builders $casestudy > $log"
+    
+    echo ">> Executing: $cmd"
+    echo "$cmd" 
+    bash -c "$cmd"
+
+    cat builders.txt|sed -r 's/[)]+/\\)/g'|sed -r 's/[(]+/\\(/g' > tmpBuilders.txt 
+
+    mv tmpBuilders.txt builders.txt
+    #Must be copy to beapi directory. Escape '('.
+    mkdir -p $scriptsdir/results-builders/$project/$casestudy/
+    cp builders.txt $scriptsdir/results-builders/$project/$casestudy/
+
+    # Rm temporary folders.
+    # rm tmpBuilders.txt
+    rm builders.txt
+    folder=${casestudy%%.*}
+    rm -r $BE_EXP_SRC/$folder
+    rm -r $BE_EXP_SRC/src/
+    # rm -r $BE_EXP_SRC/inferFiles
+    rm -r $BE_EXP_SRC/build/
+
+
+
+    popd
+}
+
+run_identification;
+
+echo "************"
+echo "Report"
+process_results_builders_display $project $casestudy;
+echo "************"
