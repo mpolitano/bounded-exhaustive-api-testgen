fe80c5fc074bb4f9b85f34263a10647d6a1169cd
valeria bengolea <vale@MacBook-Air-2.local>
2023-01-23 09:29:10 -0300

    a new (commented) repOK added to DisjSetsFast

--- 1_kiasan/src/main/java/disjointSet/fast/DisjSetsFast.java
+++ 1_kiasan/src/main/java/disjointSet/fast/DisjSetsFast.java
@@ -28,12 +28,77 @@ public class DisjSetsFast  implements java.io.Serializable{
 	 */
 	private static final long serialVersionUID = 1L;
 
-    //@ invariant "s!=null && goodValues() && acyclic();
-	 public boolean repOK() {
-		 return s!=null && goodValues() && acyclic();
+
+    //@ invariant "s!=null && goodValues() && acyclic();    
+     public boolean repOK() {
+		 
+	    return s!=null && goodValues() && acyclic(); 
 	 }
 
-	
+
+
+
+	 //A Little MORE COMPLETE REPOK: it is not part of the original structure
+     //public boolean repOK() {
+         
+       //  if(s!=null && s.length != 0 && allDiffSet()){return initial();}    
+	  //	 return s!=null &&  correctMaximunHeight(s.length)  && correctHeight()  &&  balance() && goodValues() && acyclic();
+	 //}
+
+
+
+      private boolean correctMaximunHeight(int n) {
+		 
+		 int b =  ((int)(Math.log(n) / Math.log(2)) +  1) * (-1);
+	     for( int i = 0; i < s.length; i++ ) {
+	        	if(s[i] < 0 && s[i] < b) {
+	        		return false;
+	        	}
+
+	     }
+	     return true;
+		 
+	 }
+	 
+
+
+    public int numberOfchildren(int n) {
+		 	int count = 0;
+	        for( int i = 0; i < s.length; i++ ) {
+	        	if(s[i] == n) {
+	        		count ++;
+	        	}
+	        }
+	        return count;
+	 }
+	 
+	 public boolean balance() {
+	        for( int i = 0; i < s.length; i++ ) {
+	        	if(s[i]<0) {
+	        		if (numberOfchildren(i) ==1)
+	        			return s[i] == -2;
+	        	}
+	        }
+	        return true;
+	 }
+
+
+
+
+	 public boolean allDiffSet() {
+         for( int i = 0; i < s.length; i++ )
+	        	if(s[i]>=0) {return false;}
+	        return true;
+	 }
+	 
+	 
+      public boolean initial() {
+	        for( int i = 0; i < s.length; i++ )
+	        	
+	        	if(s[i]!= -1) {return false;}
+	        return true;
+	  }
+    
 
     /**
      * Construct the disjoint sets object.
@@ -73,8 +138,8 @@ public class DisjSetsFast  implements java.io.Serializable{
     //@ requires root1 >= 0 && root1 < s.length && root2 >=0 && root2 < s.length && root1 != root2 && s[root1] < 0 && s[root2] < 0;
     //@ ensures find(root1)==find(root2);
     public void union( int root1, int root2 ){
-    	// assertIsRoot( root1 );
-    	// assertIsRoot( root2 );
+    	 assertIsRoot( root1 );
+    	 assertIsRoot( root2 );
     	if( root1 == root2 )
     		throw new IllegalArgumentException( "Union: root1 == root2 " + root1 ); 
 
@@ -127,6 +192,48 @@ public class DisjSetsFast  implements java.io.Serializable{
             }
         }
         return true;
+    }
+
+    private boolean correctHeight(){
+        for (int i = 0; i < s.length; i++) {
+            if(s[i] <0){
+                int x = heigth(i) * (-1);
+                if (x < s[i])
+                    return false;
+            }    
+        }
+        return true;
+    }
+
+
+    private int heigth(int n){
+    	if(isLeaf(n))
+    		return 1;	
+        int maxDepth = 0;
+
+        for (int i = 0; i < s.length; i++) {
+            if (s[i]==n){
+            	maxDepth = Math.max(maxDepth, heigth(i));
+                    
+            }
+        }
+        return maxDepth + 1;
+        
+    
+    }
+    
+    
+    private boolean isLeaf(int n) {
+        for (int i = 0; i < s.length; i++) {
+        	if(s[i] == n) {
+        		return false;
+        	}
+        	
+        }
+        return true;
+        
+
+    	
     }
 
     /**
